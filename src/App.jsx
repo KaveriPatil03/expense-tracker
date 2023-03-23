d996fb5927de98ad992da9d48ecb3b148bb43675
KaveriPatil03 <krpatil3012@gmail.com>
2023-03-23 14:17:44 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { HashRouter, Routes, HashRouter, Route } from "react-router-dom"
+import Landing from "./components/index"
+import Dashboard from './components/dashboard/index'
+
+const App = () => {
+  return (
+    <HashRouter>
+        <Routes>
+            {/* <Route path="/" element={<Landing />} /> */}
+            <Route path='/' element={<Dashboard />} />
+        </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
